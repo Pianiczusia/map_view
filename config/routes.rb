fc12a2aa6c48a0f5055d9e164d1d295069889020
pianiczusia <wiki.charkot@gmail.com>
2023-02-03 11:42:56 +0100

    fixing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :accounts
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
